50e2ae58743e3cae570e53aab6dc7ac459825e73
uysal-uysal <uysal.meh0@gmail.com>
2021-06-29 15:19:45 +0300

    shopping cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
@@ -19,9 +21,30 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quantity);
+        setCart(cart);
     }
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+
+        setCart(cart); 
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart); 
+    }
+
+    const handleEmptyCart= async () => {
+        const { cart } = await commerce.cart.empty();
+
+        setCart(cart); 
+    }
+
+
+
 
     useEffect(() =>{
         fetchProducts();
@@ -31,11 +54,27 @@ const App = () => {
     console.log(cart);
 
     return (
-        <div>
-            <Navbar totalItems={cart.total_items} />
-            <Products products={products} onAddToCart={handleAddToCart}/>      
-        </div>
-    )
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items} />
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart}/>      
+                    </Route>
+
+                    <Route exact path="/cart">
+                        <Cart 
+                            cart={cart} 
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+        
+    );
 }
 
 export default App
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import CartItem from './CartItem/CartItem';
+
+
+
+const Cart = ({ cart, handleEmptyCart, handleRemoveFromCart, handleUpdateCartQty}) => {
+    const classes = useStyles();
+
+    const EmptyCart = () => (
+        <Typography variant="subtitle1">Sepetiniz Boş. 
+            <Link to="/" className={classes.link}>Alışverişe Başla!</Link>
+        </Typography>
+    );
+
+    const FilledCart = () => (
+        <>  
+            <Grid container spacing = {3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs = {12} sm = {4} key={item.id}>
+                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
+                    </Grid>
+                ))}
+            </Grid>
+
+            <div className={classes.cardDetails}>
+                <Typography variant="h4">
+                    Sepet Toplamı: { cart.subtotal.formatted_with_symbol}
+                </Typography>
+                <div>
+                   <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Sepeti Temizle</Button>
+                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Siparişi Tamamla</Button>
+                </div>
+            </div>
+        </>
+    );
+
+                
+    if(!cart.line_items) return 'Loading...';
+
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} variant="h3" gutterBottom>Sepet</Typography>
+            { !cart.line_items.length ? <EmptyCart/> : <FilledCart/> }
+        </Container>  
+    )
+}
+
+export default Cart
